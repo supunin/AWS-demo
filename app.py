a3868979fa7d37053b1da01912655f60d59feabd
Supuni Nuwanthika <57209316+supunin@users.noreply.github.com>
2019-11-06 12:48:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,9 @@ def predict():
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
